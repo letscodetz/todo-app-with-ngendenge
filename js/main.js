b9496ae5bf7db3530f14be92c24eac1a91ea9dc5
mloweedgar <mloweedgar0@gmail.com>
2022-02-20 21:32:29 +0300

    nime add function yaku delete

--- js/main.js
+++ js/main.js
@@ -1,17 +1,22 @@
+let r = 0;
+
 const handleSubmit = () => {
-    const task = document.getElementById('task').value;
-    const description = document.getElementById('description').value;
-    addRow(task, description);
+    const task = document.getElementById('task');
+    const description = document.getElementById('description');
+    addRow(task.value, description.value);
+    task.value = '';
+    description.value = '';
 }
 
 
 function addRow(t, d) {
+    r++;
 
     // Get a reference to the table
     let tableRef = document.getElementById('todo-table');
 
     // Insert a row at the top of the table
-    let newRow = tableRef.insertRow(1);
+    let newRow = tableRef.insertRow(-1);
 
     // Insert a cell in the row at index 0
     let rowNo = newRow.insertCell(0);
@@ -20,7 +25,7 @@ function addRow(t, d) {
     let deleteButton = newRow.insertCell(3);
 
     // Append a text node to the cell
-    let rowNoText = document.createTextNode('1');
+    let rowNoText = document.createTextNode(r);
     rowNo.appendChild(rowNoText);
 
     let taskText = document.createTextNode(t);
@@ -31,8 +36,15 @@ function addRow(t, d) {
 
     // create a new button element
     let button = document.createElement("button");
+    button.addEventListener("click", deleteRow);
+
     let deleteButtonText = document.createTextNode('Delete');
     button.appendChild(deleteButtonText);
 
     deleteButton.appendChild(button);
+}
+
+const deleteRow = (e) => {
+    e.target.parentNode.parentNode.remove();
+    r--;
 }
