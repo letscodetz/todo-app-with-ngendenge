f1edd2771677ded30afdc95781ac7fb9e4d384b2
mloweedgar <mloweedgar0@gmail.com>
2022-02-20 19:59:35 +0300

    tume andika js logic inayo chukua data kwenye form ya todo

---
+++ js/main.js
@@ -0,0 +1,8 @@
+const handleSubmit = () => {
+    const task = document.getElementById('task').value;
+    const description = document.getElementById('description').value;
+
+    console.log('Task: ' + task);
+    console.log('Description: ' + description);
+    
+}
